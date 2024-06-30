6ea3941255b1694a7f071ccda9ae9312c4c248d4
Aditya <aditya.salunkh919@gmail.com>
2024-07-01 00:44:01 +0530

    Integrated Container networking capabilities and host-container port forwarding
    Signed-off-by: Aditya <aditya.salunkh919@gmail.com>

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 	"github.com/google/go-containerregistry/pkg/v1/tarball"
 )
 
+var exposedPorts []string
+
 func main() {
 	if len(os.Args) > 1 && os.Args[1] == "child" {
 		child()
@@ -83,8 +85,12 @@ func child() {
 			runCommand(parts[1:])
 		case "CMD":
 			runCommand(parts[1:])
+		case "EXPOSE":
+			exposePort(parts[1:])
 		}
 	}
+
+	setupNetworking()
 
 	must(syscall.Unmount("proc", 0))
 	must(syscall.Unmount("thing", 0))
@@ -126,6 +132,37 @@ func runCommand(cmd []string) {
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
 	must(command.Run())
+}
+
+func exposePort(port []string) {
+	exposedPorts = append(exposedPorts, port...)
+}
+
+func setupNetworking() {
+	// Create a veth pair
+	vethHost := "veth-host"
+	vethContainer := "veth-container"
+	must(exec.Command("ip", "link", "add", vethHost, "type", "veth", "peer", "name", vethContainer).Run())
+
+	// Move the container end of the veth pair to the container's namespace
+	must(exec.Command("ip", "link", "set", vethContainer, "netns", fmt.Sprint(os.Getpid())).Run())
+
+	// Configure the container's network interface
+	must(exec.Command("ip", "addr", "add", "192.168.1.2/24", "dev", vethContainer).Run())
+	must(exec.Command("ip", "link", "set", vethContainer, "up").Run())
+	must(exec.Command("ip", "route", "add", "default", "via", "192.168.1.1").Run())
+
+	// Bring up the host's network interface and set up a bridge
+	must(exec.Command("ip", "link", "set", vethHost, "up").Run())
+	must(exec.Command("brctl", "addif", "bridge0", vethHost).Run())
+
+	// Set up port forwarding on the host
+	for _, port := range exposedPorts {
+		hostPort := port
+		containerPort := port
+		must(exec.Command("iptables", "-t", "nat", "-A", "PREROUTING", "-p", "tcp", "--dport", hostPort, "-j", "DNAT", "--to-destination", "192.168.1.2:"+containerPort).Run())
+		must(exec.Command("iptables", "-t", "nat", "-A", "POSTROUTING", "-j", "MASQUERADE").Run())
+	}
 }
 
 func must(err error) {
