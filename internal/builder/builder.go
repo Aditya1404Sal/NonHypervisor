4aa530e7f824c54936e9b52be83f1ff2bc01d17c
Aditya <aditya.salunkh919@gmail.com>
2024-07-08 21:54:53 +0530

    feat(builder): Added Layer building and hash caching functionality similar to docker build
    Signed-off-by: Aditya <aditya.salunkh919@gmail.com>

--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -1,10 +1,47 @@
 package builder
 
 import (
+	"Nonhypervisor/internal/cache"
 	"Nonhypervisor/internal/config"
+	"Nonhypervisor/internal/layer"
+	"fmt"
 )
 
 // BuildImage builds an image based on the configuration
 func BuildImage(cfg *config.Config) error {
+	fmt.Printf("Building image from base: %s\n", cfg.BaseImage)
 
+	for _, l := range cfg.Layers {
+		layerHash, err := layer.HashLayer(l)
+		if err != nil {
+			return fmt.Errorf("error hashing layer: %v", err)
+		}
+
+		if cache.CheckCache(layerHash) {
+			fmt.Printf("Layer %s found in cache, skipping...\n", layerHash)
+			continue
+		}
+
+		if l.Run != "" {
+			fmt.Printf("Running command: %s\n", l.Run)
+			err = layer.RunCommand(l.Run)
+			if err != nil {
+				return fmt.Errorf("error running command: %v", err)
+			}
+		} else if l.Copy.Src != "" && l.Copy.Dest != "" {
+			fmt.Printf("Copying files from %s to %s\n", l.Copy.Src, l.Copy.Dest)
+			err = layer.CopyFiles(l.Copy.Src, l.Copy.Dest)
+			if err != nil {
+				return fmt.Errorf("error copying files: %v", err)
+			}
+		}
+
+		// Simulate storing the layer in cache
+		err = cache.StoreLayer(layerHash, []byte("layer data"))
+		if err != nil {
+			return fmt.Errorf("error storing layer in cache: %v", err)
+		}
+	}
+
+	return nil
 }
--- internal/layer/layer.go
+++ internal/layer/layer.go
@@ -5,6 +5,8 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"os"
+	"os/exec"
 )
 
 // HashLayer generates a SHA-256 hash for a layer
@@ -15,4 +17,20 @@ func HashLayer(layer interface{}) (string, error) {
 		return "", err
 	}
 	return hex.EncodeToString(hash.Sum(nil)), nil
+}
+
+// RunCommand runs a shell command and returns any errors encountered
+func RunCommand(command string) error {
+	cmd := exec.Command("sh", "-c", command)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+// CopyFiles copies files from the source to the destination
+func CopyFiles(src, dest string) error {
+	cmd := exec.Command("cp", "-r", src, dest)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
